6e0838dee218a60d8371eff5fa650d0f2452f525
Pruthvi <pruthvisoni0505@gmail.com>
2023-06-08 20:34:33 -0400

    Add project files.

---
+++ Project_Final_Pruthvi/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_Final_Pruthvi;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Project_Final_Pruthvi/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Project_Final_Pruthvi;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
